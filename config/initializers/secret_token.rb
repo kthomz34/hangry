cd8522ee8f828205a9ca3b6a10e09fccfb902fdf
Kurt Thomas <kthomz34@icloud.com>
2015-10-28 22:19:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangry::Application.config.secret_key_base = 'ff39b0389b4f681a88905a9b844154855ef3eb389e25d4f696eca99eb612bd32facf2f047cef9855f8ae43f967bbdf504ede06a43de16bcf1952dc3fe2086970'
